51aa37f05520474ed46352a8631b5823376538da
kaeul991020 <kaeul991020@gmail.com>
2022-11-15 15:33:11 +0900

    Refactor : adminlte3_theme app 이름 변경
    - adminlte3_theme를 adminpage로 변경함.
    
    related to : #52 #29
    close : #52

--- SolarProject/settings.py
+++ SolarProject/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ['127.0.0.1']
 
 INSTALLED_APPS = [
     'adminlte3',
-    'adminlte3_theme',
+    'adminpage',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
