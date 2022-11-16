6b4dbaa1a404de36da7aeddebdd541ab8b009580
kaeul991020 <kaeul991020@gmail.com>
2022-11-16 16:50:30 +0900

    Feat : 비밀번호 초기화와 관련된 페이지 추가 생성
    - auth_views.PasswordResetView 생성
    - auth_views.PasswordResetDoneView 생성
    - auth_views.PasswordResetConfirmView 생성
    - auth_views.PasswordResetCompleteView 생성
    - urls.py에서 비밀번호 초기화와 관련된 url들을 password_reset_patterns으로 따로 분리하기
    
    related to : #21 #57

--- login/urls.py
+++ login/urls.py
@@ -4,11 +4,20 @@ from django.conf.urls import url, include
 from django.contrib.auth import views as auth_views
 from django.views.generic.base import TemplateView
 
+ 
+password_reset_patterns=[
+    path('', auth_views.PasswordResetView.as_view(template_name='login/forgot-password.html')),
+    path('done/', auth_views.PasswordResetDoneView.as_view()),
+    path('confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view()),
+    path('complete/', auth_views.PasswordResetCompleteView.as_view(template_name='login/forgot-password-success.html'))
+]
 
 urlpatterns=[
     path('',auth_views.LoginView.as_view(template_name='login/login.html')),
-    path('forgot-password/', TemplateView.as_view(template_name='login/forgot-password.html')),
-    path('forgot-password-success/', TemplateView.as_view(template_name='login/forgot-password-success.html')),
+    path('password_reset/', include(password_reset_patterns)),
     path('register/', TemplateView.as_view(template_name='login/register.html')),
     path('register-success/', TemplateView.as_view(template_name='login/register-success.html')),
 ]
+
+
+
