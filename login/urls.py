3b71316f3a17871c02de031ff953ee4d047a729b
kaeul991020 <kaeul991020@gmail.com>
2022-11-16 17:23:10 +0900

    Refactor : register-success.html 이름 변경
    - password_done과 통일성을 주기 위해 변경함.
    - register-success.html을 register_done.html으로 변경함.
    
    related to : #21 #57

--- login/urls.py
+++ login/urls.py
@@ -4,19 +4,23 @@ from django.conf.urls import url, include
 from django.contrib.auth import views as auth_views
 from django.views.generic.base import TemplateView
 
- 
+
 password_reset_patterns=[
-    path('', auth_views.PasswordResetView.as_view(template_name='login/password_reset.html')),
-    path('done/', auth_views.PasswordResetDoneView.as_view()),
-    path('confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view()),
+    path('', auth_views.PasswordResetView.as_view(template_name='login/password_reset.html'), name='password_reset'),
+    path('done/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('complete/', auth_views.PasswordResetCompleteView.as_view(template_name='login/password_reset_complete.html'))
 ]
 
+register_patterns=[
+    path('', TemplateView.as_view(template_name='login/register.html')),
+    path('done/', TemplateView.as_view(template_name='login/register_done.html')),
+]
+
 urlpatterns=[
-    path('',auth_views.LoginView.as_view(template_name='login/login.html')),
+    path('',auth_views.LoginView.as_view(template_name='login/login.html'), name='login'),
     path('password_reset/', include(password_reset_patterns)),
-    path('register/', TemplateView.as_view(template_name='login/register.html')),
-    path('register-success/', TemplateView.as_view(template_name='login/register-success.html')),
+    path('register/',  include(register_patterns)),
 ]
 
 
