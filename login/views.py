afe09ae75913d98160f2b1f7dd7c933e19209836
kaeul991020 <kaeul991020@gmail.com>
2022-11-17 20:29:19 +0900

    Style : login.views 내용 정리
    - login의 views.py의 모든 코드가 주석처리 되어있었기 때문에 삭제함.

--- login/views.py
+++ login/views.py
@@ -1,12 +1,3 @@
 from django.shortcuts import render
-from django.contrib.auth import views as auth_views
-from django.views.generic.base import TemplateView
 
 # Create your views here.
-
-'''class password_reset_confirm(auth_views.PasswordResetConfirmView, TemplateView):
-    success_url = 'login:password_reset_done'''
-
-
-
-
