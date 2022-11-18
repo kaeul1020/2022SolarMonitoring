a7e547dfdcc621de878bfc16dc35399aa9065d3d
kaeul991020 <kaeul991020@gmail.com>
2022-11-18 16:25:44 +0900

    Feat : 회원가입에서 email이 겹치지 않도록 validation 설정
    - 회원가입 페이지에서 기존 사용자와 새로 가입하려는 사용자의  e-mail 주소가 겹치지 않도록 validation을 설정했다.
    
    related to : #61 #19

--- login/forms.py
+++ login/forms.py
@@ -9,4 +9,17 @@ class UserForm(UserCreationForm):
         model = User
         fields = ("username", "password1", "password2", "email")
 
+    def clean_email(self):
+        email=self.cleaned_data.get('email')
+        try:
+            User.objects.get(email=email)
+            raise forms.ValidationError("해당 e-mail 주소는 이미 존재합니다.")
+        except User.DoesNotExist:
+            return email
+    
+
+
+            
+
+
 ##, "first_name", "last_name"
