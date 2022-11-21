713ad8476ff5e8552d2895856c2b372bfa305e98
kaeul991020 <kaeul991020@gmail.com>
2022-11-21 16:15:16 +0900

    Feat : FAQ 기능 구현
    - faq를 아코디언 형식으로 구현함.
    - dbo.faq_faqmodel과 화면을 연결함.
    
    related to : #66 #68
    close : #66 #68

--- faq/urls.py
+++ faq/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.views.generic.base import TemplateView
+from . import views as FaqView
 
 
 app_name = 'faq'
 
 urlpatterns=[
-    path('', TemplateView.as_view(template_name='faq/faq.html'), name='faq'),
+    path('', FaqView.FaqView.as_view(template_name='faq/faq.html'), name='faq'),
 ]
 
--- faq/views.py
+++ faq/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import FaqModel
+from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-# Create your views here.
+class FaqView(TemplateView):
+    queryset = FaqModel.objects.all()  
+
+    def get(self, request, *args, **kwargs):
+        ctx = {
+            'FAQs': self.queryset 
+        }
+        return self.render_to_response(ctx)
+
+
