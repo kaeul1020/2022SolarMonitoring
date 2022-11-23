1874c2dcc0aa8712bbf331b9834cb2acc859a4ef
kaeul991020 <kaeul991020@gmail.com>
2022-11-23 15:56:15 +0900

    Style : password_reset_confirm UI 생성
    - password_reset_confirm.html을 생성함.
    - urls.py로 password_reset_confirm.html을 연결함.
    
    related to : #21 #35
    close : #21 #35

--- login/urls.py
+++ login/urls.py
@@ -12,7 +12,9 @@ password_reset_patterns=[
     path('', login_views.UserPasswordResetView.as_view(),name='password_reset'),
     path('done/', 
         auth_views.PasswordResetDoneView.as_view(template_name='login/password_reset_done.html'),name="password_reset_done"),
-    path('confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(success_url=reverse_lazy('login:password_reset_complete')), name='password_reset_confirm'),
+    path('confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        success_url=reverse_lazy('login:password_reset_complete'),
+        template_name='login/password_reset_confirm.html'), name='password_reset_confirm'),
     path('complete/', auth_views.PasswordResetCompleteView.as_view(template_name='login/password_reset_complete.html'), name='password_reset_complete')
 ]
 
