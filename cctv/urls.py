4186b76366df247a868ac519cc15f1d5a9ffea0e
kaeul991020 <kaeul991020@gmail.com>
2022-11-29 13:28:34 +0900

    Design : 실시간 패널 오염감지 CCTV 패널 설정 화면 UI 생성
    - 실시간 패널 오염감지 CCTV 패널 설정 화면 UI를 만들기 위해 cctv_setting.html 생성
    - 패널사진을 가져오기 위해 새로운 cctv 이미지들 추가
    - cctv와 cctv_setting 연결
    
    related to : #76 #84 #88

--- cctv/urls.py
+++ cctv/urls.py
@@ -7,4 +7,5 @@ app_name = 'cctv'
 
 urlpatterns=[
     path('', TemplateView.as_view(template_name='cctv/cctv.html'), name='cctv'),
+    path('setting', TemplateView.as_view(template_name='cctv/cctv_setting.html'), name='cctv_setting'),
 ]
