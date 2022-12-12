97e9a4aff45bcc6da178db1e9e98fabbc640d262
kaeul991020 <kaeul991020@gmail.com>
2022-12-13 00:50:28 +0900

    Design : cctv화면 status_box 통일
    - cctv화면의 status_box를 통일하기 위해 코드 분리
    
    related to : #84 #76
    close : #84

--- cctv/views.py
+++ cctv/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+status= [
+    { "title": "오염 유형", "value": "나뭇잎, 흙먼지", "unit": "", "icon_color":"bg-info","icon_class": "fas fa-broom" },
+    { "title": "예상 발전 감소량", "value": "35", "unit": "MWh", "icon_color":"bg-info","icon_class": "fas fa-bolt" },
+    { "title": "오염된 패널 수", "value": "4", "unit": "/ 4개", "icon_color":"bg-info","icon_class": "fas fa-solar-panel" },
+    { "title": "오염 레벨", "value": "경고", "unit": "", "icon_color":"bg-warning","icon_class": "fas fa-sad-tear" },
+]
+
+
+
+class cctv(TemplateView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['status_box'] = status
+        return context
