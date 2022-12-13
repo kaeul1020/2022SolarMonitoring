4948afac7c0b2dd1cc7c4950082cc3adf03f08a5
kaeul991020 <kaeul991020@gmail.com>
2022-12-13 17:47:57 +0900

    Feat : alarm 화면 data와 html 분리
    - html 내부에 쓰여있던 데이터를 views.py쪽으로 분리시킴.

--- alarm/urls.py
+++ alarm/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.views.generic.base import TemplateView
+from . import views as alarm_view
 
 
 app_name = 'alarm'
 
 urlpatterns=[
-    path('', TemplateView.as_view(template_name='alarm/alarm.html'), name='alarm'),
+    path('', alarm_view.alarm.as_view(template_name='alarm/alarm.html'), name='alarm'),
 ]
